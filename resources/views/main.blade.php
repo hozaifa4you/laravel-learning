63da14a6a9b08055a6532ab5a72a17925db163a1
Hozaifa Bin Masud <60110696+hozaifa4you@users.noreply.github.com>
2023-12-05 04:55:16 +0000

    Blade Template

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,13 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welcome Back</title>
-</head>
-<body>
-    <h2>How Are You Mr. Yousuf Ahamad</h2>
-    <a href="/posts">Go to posts</a>
-</body>
-</html>
+<h2>Main Page</h2>
+
+@php
+    $users = ['Yousuf Ahamad', 'Anamul Hasan', 'Amma Anisa', 'Anika'];
+@endphp
+
+
+<ul>
+    @foreach ($users as $user)
+        <li>{{ $user }}</li>
+    @endforeach
+</ul>
