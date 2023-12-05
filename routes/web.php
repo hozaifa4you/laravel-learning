5fd4bd46717799e6e302fe2cc482565d42646f5c
Hozaifa Bin Masud <60110696+hozaifa4you@users.noreply.github.com>
2023-12-05 17:48:06 +0000

    Blade template 4

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
+$user = [
+    "name" => "Yousuf Ahamd",
+    "email" => "yousuf360.ya@gmail.ocm",
+    "age" => 28,
+    "mobile" => "01918780770",
+    "address" => "Satkhira",
+];
+
 Route::get('/', function () {
     return view('pages.home');
 })->name('home');
@@ -22,18 +30,16 @@ Route::prefix('posts')->group(function () {
     Route::get('/', function () {
         return view('pages.posts');
     })->name('posts');
+
     // get single post vie id
     Route::get('/{post_id?}', function (
         string $post_id
     ) {
-
-        $post_title = 'What is Lorem Ipsum?';
-        $post_details = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-
-        return view('post', ['postId' => $post_id, 'postTitle' => $post_title, 'postDetails' => $post_details]);
+        return view('pages.post');
     })
         ->whereAlphaNumeric('post_id')
         ->name("post");
+
     // create new post
     Route::get('/create', function () {
         return view('create-post');
@@ -47,6 +53,20 @@ Route::get('/about', function () {
 Route::get('/contact', function () {
     return view('pages.contact');
 })->name('contact');
+
+Route::get('/user', function () {
+    $user = [
+        "name" => "Yousuf Ahamd",
+        "email" => "yousuf360.ya@gmail.ocm",
+        "age" => 28,
+        "mobile" => "01918780770",
+        "address" => "Satkhira",
+        "brothers" => ["Anamul Hasan", "Ather Rahman", "Ibrahim Khalil", "Niloy Jamil", "Abu Huraira"],
+        "city" => ["Dhaka", "Chattogram", "Satkhira", "Khulna"]
+    ];
+
+    return view('pages.user', ['user' => $user]);
+})->name('user');
 
 
 
