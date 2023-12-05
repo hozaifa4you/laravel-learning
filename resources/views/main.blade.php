23178062539c37923b86d45556ee700366851c0e
Hozaifa Bin Masud <60110696+hozaifa4you@users.noreply.github.com>
2023-12-05 05:42:07 +0000

    Blade Template 2

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,12 +1,21 @@
+@include('partials.header')
+
+<title>Home - Laravel Learning</title>
+
 <h2>Main Page</h2>
 
 @php
     $users = ['Yousuf Ahamad', 'Anamul Hasan', 'Amma Anisa', 'Anika'];
+    $rivers = ['padma', 'meghna', 'jomuna', 'posur'];
 @endphp
 
+@include('partials/first', ['user' => $users])
+@include('partials.second')
 
 <ul>
     @foreach ($users as $user)
         <li>{{ $user }}</li>
     @endforeach
 </ul>
+
+@include('partials.footer')
---
+++ resources/views/partials/first.blade.php
@@ -0,0 +1,6 @@
+<h4>This is First Template</h4>
+<ul>
+    @foreach ($users as $user)
+        <li>First Page => {{ $user }}</li>
+    @endforeach
+</ul>
