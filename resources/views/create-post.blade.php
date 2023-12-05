c77518b83739e85898ec34b33866d8b466f00b4b
Hozaifa Bin Masud <60110696+hozaifa4you@users.noreply.github.com>
2023-12-05 03:50:08 +0000

    Routing group

---
+++ resources/views/create-post.blade.php
@@ -0,0 +1,4 @@
+<h2>Create New Post</h2>
+<a href="{{route('posts')}}">Back your posts</a>
+
+<h4>Fill all form to create new post</h4>
--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,34 @@ Route::get('/', function () {
     return view('main');
 })->name('home');
 
-Route::get('/posts', function () {
-    return view('posts');
-})->name('posts');
+Route::prefix('posts')->group(function () {
+    // get all posts
+    Route::get('/', function () {
+        return view('posts');
+    })->name('posts');
+    // get single post vie id
+    Route::get('/{post_id?}', function (
+        string $post_id
+    ) {
 
-Route::get('/posts/{post_id?}', function (
-    string $post_id = null,
-) {
+        $post_title = 'What is Lorem Ipsum?';
+        $post_details = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
-    $post_title = 'What is Lorem Ipsum?';
-    $post_details = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+        return view('post', ['postId' => $post_id, 'postTitle' => $post_title, 'postDetails' => $post_details]);
+    })
+        ->whereAlphaNumeric('post_id')
+        ->name("post");
+    // create new post
+    Route::get('/create', function () {
+        return view('create-post');
+    });
+});
 
-    return view('post', ['postId' => $post_id, 'postTitle' => $post_title, 'postDetails' => $post_details]);
-})
-    ->whereNumber('post_id')
-    ->name("post");
+
+
+
+
+Route::redirect("/say-hi", '/', 301);
+Route::fallback(function () {
+    return view('notfund');
+});
