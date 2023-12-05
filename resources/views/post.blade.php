6725ae50bd73491d86f53429d85a5b393e605af1
Hozaifa Bin Masud <60110696+hozaifa4you@users.noreply.github.com>
2023-12-05 03:22:56 +0000

    router name concept

---
+++ resources/views/post.blade.php
@@ -0,0 +1,12 @@
+<title>Post Details</title>
+
+<h2>{{$postTitle}}</h2>
+
+<a href="{{ route("posts") }}">Back to posts</a>
+
+<span>{{$postId}}</span>
+
+
+<p>
+    {{$postDetails}}
+</p>
--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('main');
-});
+})->name('home');
 
 Route::get('/posts', function () {
     return view('posts');
-});
+})->name('posts');
 
-Route::get('/posts/{post_id}/comments/{comment_id}', function (string $post_id = null, string $comment_id = null) {
-    return 'Post Id = ' . $post_id . " comment id = " . $comment_id;
-})->whereNumber('post_id')->whereAlpha('comment_id');
+Route::get('/posts/{post_id?}', function (
+    string $post_id = null,
+) {
+
+    $post_title = 'What is Lorem Ipsum?';
+    $post_details = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+    return view('post', ['postId' => $post_id, 'postTitle' => $post_title, 'postDetails' => $post_details]);
+})
+    ->whereNumber('post_id')
+    ->name("post");
